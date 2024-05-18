7c661ae5d355b77f42d36c938a8a114a47137aef
Jacek programmer <jacek301@gmail.com>
2024-05-19 01:10:58 +0200

    Expressions Matter Kata start

---
+++ src/expression/matter.clj
@@ -0,0 +1,15 @@
+(ns expression.matter)
+
+(defn multi [a b c]
+  (* a b c)
+  )
+
+(defn expression-matter [a b c]
+
+  (multi a b c)
+
+  ;(* a b c)
+  ;(* a (+ b c))
+
+
+  )
--- test/fundamentals_kata_training_clojure/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns fundamentals-kata-training-clojure.core-test
-  (:require [clojure.test :refer :all]
-            [fundamentals-kata-training-clojure.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
