172be609777d032b9fbc5122764d62ad2cc934e3
Jacek programmer <jacek301@gmail.com>
2024-05-21 23:26:34 +0200

    Rock Paper Scissors! kata Solution with library  match

--- src/rock_paper_scissors.clj
+++ src/rock_paper_scissors.clj
@@ -1,4 +1,24 @@
-(ns rock-paper-scissors)
+(ns rock-paper-scissors
+  (:require [clojure.core.match :refer [match]]))
+
+(defn player_2_winner []
+  "Player 2 won!"
+  )
+
+(defn player_1_winner []
+  "Player 1 won!"
+  )
 
 (defn rps [p1 p2]
+
+
+  (match [p1 p2]
+         ["scissors" "rock"] (player_2_winner)
+         ["paper" "scissors"] (player_2_winner)
+         ["rock" "paper"] (player_2_winner)
+         ["rock" "scissors"] (player_1_winner)
+         ["scissors" "paper"] (player_1_winner)
+         ["paper" "rock"] (player_1_winner)
+         [_ _] "Draw!"
+         )
   )
