011b8e7b6a180021f4f7ef0ee0bb6a2022b0579c
Jacek programmer <jacek301@gmail.com>
2024-05-22 01:13:49 +0200

    Rock Paper Scissors! kata Solution another not mine

--- src/rock_paper_scissors.clj
+++ src/rock_paper_scissors.clj
@@ -1,40 +1,12 @@
 (ns rock-paper-scissors)
 
-(defn player_2_winner []
-  "Player 2 won!"
-  )
-
-(defn player_1_winner []
-  "Player 1 won!"
-  )
-
-(defn draw []
-  "Draw!"
-  )
-
 (defn rps [p1 p2]
-
-  (if (= p1 p2)
-    (draw)
-    (if (and (= p1 "scissors") (= p2 "rock"))
-      (player_2_winner)
-      (if (and (= p1 "paper") (= p2 "scissors"))
-        (player_2_winner)
-        (if (and (= p1 "rock") (= p2 "paper"))
-          (player_2_winner)
-          (if (and (= p1 "rock") (= p2 "scissors"))
-            (player_1_winner)
-            (if (and (= p1 "scissors") (= p2 "paper"))
-              (player_1_winner)
-              (if (and (= p1 "paper") (= p2 "rock"))
-                (player_1_winner)
-                )
-              )
-            )
-          )
-        )
-      )
-
-    )
-  )
+  (cond
+    (= p1 p2) "Draw!"
+    (case [p1 p2]
+      ["scissors" "paper"] true
+      ["paper" "rock"] true
+      ["rock" "scissors"] true
+      false) "Player 1 won!"
+    :else "Player 2 won!"))
 
