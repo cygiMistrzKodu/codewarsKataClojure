4001e2f125e815f56b1318c506f9587f1ee9cd4b
Jacek programmer <jacek301@gmail.com>
2024-05-22 17:47:18 +0200

    Regex count lowercase letters kata Solution 3

--- src/kata/lowercase_count.clj
+++ src/kata/lowercase_count.clj
@@ -1,10 +1,7 @@
 (ns kata.lowercase-count)
 
-(defn is_lowercase? [letter]
-  (Character/isLowerCase letter)
-  )
-
 (defn lowercase_count [strng]
 
-  (count (re-seq #"[a-z]" strng))
+  (def lowercase (re-seq #"[a-z]" strng))
+  (count lowercase)
   )
